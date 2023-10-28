c0bb56e4d87cd6311fc6c3411ef9de39e318331f
Akram Hossain <akram686202@gmail.com>
2023-10-28 11:51:18 +0600

    first commit

---
+++ src/app/pages/setting/colour/colour.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ColourRoutingModule } from './colour-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ColourRoutingModule
+  ]
+})
+export class ColourModule { }
---
+++ src/app/pages/setting/size/size.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SizeRoutingModule } from './size-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SizeRoutingModule
+  ]
+})
+export class SizeModule { }
