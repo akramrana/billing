4698bde93bc9a53c363b24b61450bd9324bd119e
Akram Hossain <akram686202@gmail.com>
2023-11-09 14:52:45 +0600

    changes

--- src/app/pages/order/order-view/order-view.component.ts
+++ src/app/pages/order/order-view/order-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { first } from 'rxjs/operators';
@@ -29,17 +29,36 @@ export class OrderViewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.getStatus();
     this.title = 'View';
     this._route.params.subscribe((param) => {
       this.id = (param['id']) ? param['id'] : 0;
     });
     if (this.id) {
-      this.title = 'Update';
-      this.getUpdateData();
+      this.getViewData();
+      this.formGroup = this.fb.group({
+        orderId: [this.id],
+        statusId: [null, Validators.required],
+        comment: [],
+      });
     }
   }
 
-  getUpdateData() {
+  getStatus() {
+    this.statusList = [];
+    this.apiService.readRequest(this.dropdownRoute, {
+      _route: "status-list"
+    })
+      .pipe(first())
+      .subscribe(response => {
+        const _data = response.body;
+        if (_data) {
+          this.statusList = _data;
+        }
+      });
+  }
+
+  getViewData() {
     this.apiService.readRequest(this.baseRoute, {
       id: this.id,
       _route: "view"
@@ -52,6 +71,35 @@ export class OrderViewComponent implements OnInit {
           this.order = _data;
         }
       });
+  }
+
+  changeStatus($event: any) {
+    Object.keys(this.formGroup.controls).forEach(field => {
+      const control = this.formGroup.get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
+    if (this.formGroup.valid) {
+      try {
+        const postParams = this.formGroup.value;
+        //console.log(postParams);
+        this.apiService.postRequest(this.baseRoute, { _route: 'change-status' }, postParams)
+          .pipe(first())
+          .subscribe(response => {
+            if (response.status === 1) {
+              this.toastrService.success('Status updated successfully!');
+              this.formGroup.patchValue({
+                statusId: "",
+                comment: ""
+              });
+              this.getViewData();
+            } else if (response.status === 0) {
+              this.toastrService.error(response.message);
+            }
+          });
+      } catch (e) {
+        // console.log(e);
+      }
+    }
   }
 
 }
